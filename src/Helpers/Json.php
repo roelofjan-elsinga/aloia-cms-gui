8fa30bf159b897bf190f747b7ba0eb7384928f2c
Roelof Jan Elsinga <roelofjanelsinga@gmail.com>
2020-07-12 20:45:06 +0200

    Fixed variable name

--- src/Helpers/Json.php
+++ src/Helpers/Json.php
@@ -6,8 +6,8 @@ class Json
 {
     public static function isValid(string $value): bool
     {
-        if (!empty($data)) {
-            @json_decode($data);
+        if (!empty($value)) {
+            @json_decode($value);
             return (json_last_error() === JSON_ERROR_NONE);
         }
 
