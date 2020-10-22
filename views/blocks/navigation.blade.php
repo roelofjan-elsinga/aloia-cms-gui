82882c3d1c81393a52e803f5e7e431dd6a99870d
Roelof Jan Elsinga <roelofjanelsinga@gmail.com>
2020-10-22 11:41:54 +0200

    Fixed mobile menu

--- views/blocks/navigation.blade.php
+++ views/blocks/navigation.blade.php
@@ -1,9 +1,9 @@
-<aside x-data="{open:false}" class="w-screen md:w-64">
-    <div class="md:hidden text-right" x-show="!open">
-        <button @click="open = true" class="mt-8">Menu</button>
+<aside x-data="{open:false}" class="w-full h-screen md:w-64 fixed z-10">
+    <div class="md:hidden relative pr-4" x-show="!open">
+        <button @click="open = true" class="mt-8 float-right">Menu</button>
     </div>
 
-    <nav class="w-screen h-screen md:w-64 bg-gray-100 p-8 -mx-4 md:mx-0 absolute md:relative md:block"
+    <nav class="w-full h-full md:w-64 bg-gray-100 p-8 md:mx-0 absolute md:relative md:block z-10"
          :class="{'block' : open, 'hidden': !open}">
         <a class="text-blue-darkest no-underline text-2xl font-bold hidden md:block mb-8"
            href="{{ url($dashboard_url) }}">
@@ -62,7 +62,7 @@
             </form>
         @endif
 
-        <a class="text-green-900 bg-green-200 block font-bold px-8 py-4 rounded mt-8"
+        <a class="text-green-900 bg-green-200 block font-bold px-8 py-4 rounded mt-8 text-center"
            href="{{ url($website_url) }}">
             {{trans('aloiacmsgui::interface.view_website')}}
         </a>
